7c1d8b744de6c4aee078bc45f0a3e79d77faa14f
chaocai <chaocai@192.168.3.44>
2023-01-13 22:19:50 +0800

    updated version

--- examples_test.go
+++ examples_test.go
@@ -10,10 +10,11 @@ import (
 )
 
 func TestAsynExecutor(t *testing.T) {
+	retArry := [5]int{1, 2, 3, 4, 5}
 	TimeCosumingOps := func(ctx context.Context) concurrency.TaskResult {
 		time.Sleep(time.Second * 1)
 		return concurrency.TaskResult{
-			"Hello Asynchrony", nil,
+			&retArry, nil,
 		}
 	}
 	// We can invok time-cosuming firstly.
@@ -22,5 +23,10 @@ func TestAsynExecutor(t *testing.T) {
 	// Do something else.
 	fmt.Println("Do something else")
 	// Get the results while we just need it
-	fmt.Println("Now, we need the result", retStub.GetResult().Result)
+	if retStub.GetResult().Err != nil {
+		fmt.Println("The error is", retStub.GetResult().Err)
+		t.Error(retStub.GetResult().Err)
+	}
+	ret, _ := retStub.GetResult().Result.(*[5]int)
+	fmt.Println("Now, we need the result", *ret)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/easierway/concurrency_utils_examples
 
 go 1.13
 
-require github.com/easierway/concurrency_utils v0.6.6 
+require github.com/easierway/concurrency_utils v0.9.0
